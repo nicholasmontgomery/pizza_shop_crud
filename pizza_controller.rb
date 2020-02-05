8ec3e40699adaf1a3a87e84ff7ab292ea25eb139
FKeir <fmkeir@gmail.com>
2020-02-05 13:45:34 +0000

    Initial commit

---
+++ pizza_controller.rb
@@ -0,0 +1,27 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/pizza_order')
+also_reload('./models/*')
+
+
+get "/pizza_orders" do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+get "/pizza_orders/new" do
+  erb(:new)
+end
+
+get "/pizza_orders/:id" do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+post "/pizza_orders" do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+  # redirect to "/pizza_orders"
+end
